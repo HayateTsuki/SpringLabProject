b4d3c17be5255c8a4775c58ea2d152f95945ea0a
Mateusz <129283151+HayateTsuki@users.noreply.github.com>
2023-10-01 12:38:45 +0200

    Initial setup commit

---
+++ src/test/java/lab/projektSpring/SpringLabProject/SpringLabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.projektSpring.SpringLabProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
